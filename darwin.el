0745e5d076330d4ead366dc3ef75167b322c1029
Bradley Wright <brad@intranation.com>
2010-02-27 13:02:28 +0000

    Figured out how to nest multiple things, with Hunter's help

--- darwin.el
+++ darwin.el
@@ -1,10 +1,10 @@
 ;; OS X Specific configuration
 
-;; Free up the option key for special characters
+;; When we have a window system, do some specific stuff
 (when window-system
-  (setq ns-alternate-modifer (quote none)))
-(when window-system
-  (setq ns-command-modifier (quote meta)))
-;; set my favourite Mac font as the default font
-(when window-system
-  (set-face-font 'default "-apple-inconsolata-medium-r-normal--15-150-72-72-m-150-iso10646-1"))
+  (progn
+    ;; Free up the option key for special characters
+    (setq ns-alternate-modifer (quote none))
+    (setq ns-command-modifier (quote meta))
+    ;; set my favourite Mac font as the default font
+    (set-face-font 'default "-apple-inconsolata-medium-r-normal--15-150-72-72-m-150-iso10646-1")))
