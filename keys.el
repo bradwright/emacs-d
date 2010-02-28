db8db903088d4a3951cd6f2e46f92691cef6d0eb
Bradley Wright <brad@intranation.com>
2010-02-28 21:34:34 +0000

    Don't need to escape that...

--- keys.el
+++ keys.el
@@ -5,7 +5,7 @@
 
 ;; Use regex searches by default.
 (global-set-key (kbd "C-s") 'isearch-forward-regexp)
-(global-set-key (kbd "\C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-r") 'isearch-backward-regexp)
 (global-set-key (kbd "C-M-s") 'isearch-forward)
 (global-set-key (kbd "C-M-r") 'isearch-backward)
 
