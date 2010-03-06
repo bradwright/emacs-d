127e60f38edb65b710c1e5593c9065399d7364e2
Bradley Wright <brad@intranation.com>
2010-03-06 11:25:09 +0000

    Get color theme working

---
+++ gui.el
@@ -0,0 +1,6 @@
+;; Color themes and graphical embellishment
+
+(require 'color-theme)
+(setq color-theme-is-global t)
+(load-file "~/Projects/emacs-d/dist/elisp/color-theme-twilight.el")
+(color-theme-twilight)
