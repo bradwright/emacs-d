75e585de442abd7a5d33365ddc124fb8995bdb6a
Bradley Wright <brad@intranation.com>
2010-03-28 20:41:47 +0100

    Some Elisp functions (mostly stolen from starter kit)

---
+++ elisp.el
@@ -0,0 +1,6 @@
+(defun recentf-ido-find-file ()
+  "Find a recent file using ido."
+  (interactive)
+  (let ((file (ido-completing-read "Choose recent file: " recentf-list nil t)))
+    (when file
+      (find-file file))))
