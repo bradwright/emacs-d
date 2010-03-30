6326af7308b920ca91a06096e5c3fa1d8a0896ca
Bradley Wright <brad@intranation.com>
2010-03-30 20:03:46 +0100

    Move files to vendor

--- init.el
+++ init.el
@@ -3,7 +3,11 @@
 ;; Intended to be robust and usable across platforms
 
 ;; Load external files
-(add-to-list 'load-path "~/.emacs-d/dist/elisp")
+(setq dotfiles-dir (file-name-directory
+                    (or (buffer-file-name) load-file-name)))
+
+(add-to-list 'load-path dotfiles-dir)
+(add-to-list 'load-path (concat dotfiles-dir "/vendor"))
 
 ;; load on startup
 (require 'cl)
