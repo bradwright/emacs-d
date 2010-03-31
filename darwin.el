33adeff6454ef4d93289f97483c37bed56f6d6c2
Bradley Wright <brad@intranation.com>
2010-03-31 09:16:21 +0100

    Add window swapping

--- darwin.el
+++ darwin.el
@@ -8,3 +8,8 @@
     (setq ns-command-modifier (quote meta))
     ;; set my favourite Mac font as the default font
     (set-face-font 'default "-apple-inconsolata-medium-r-normal--15-150-72-72-m-150-iso10646-1")))
+;; Free up the option key for special characters
+(setq ns-alternate-modifer (quote none))
+(setq ns-command-modifier (quote meta))
+;; set default startup
+(setq default-directory "~")
