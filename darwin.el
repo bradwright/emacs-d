4dd2f93efa806817a3823a38f74c5f58fb356ded
Bradley Wright <brad@intranation.com>
2010-03-31 09:16:49 +0100

    Remove these shortcuts from window-system

--- darwin.el
+++ darwin.el
@@ -3,9 +3,6 @@
 ;; When we have a window system, do some specific stuff
 (when window-system
   (progn
-    ;; Free up the option key for special characters
-    (setq ns-alternate-modifer (quote none))
-    (setq ns-command-modifier (quote meta))
     ;; set my favourite Mac font as the default font
     (set-face-font 'default "-apple-inconsolata-medium-r-normal--15-150-72-72-m-150-iso10646-1")))
 ;; Free up the option key for special characters
