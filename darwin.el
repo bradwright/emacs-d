cc3be4afd821d3584ef959e98696c1be68496baa
Bradley Wright <brad@intranation.com>
2010-04-01 23:16:33 +0100

    Move OS X specific key bindings to a window only elisp

--- darwin.el
+++ darwin.el
@@ -4,10 +4,10 @@
 (when window-system
   (progn
     ;; set my favourite Mac font as the default font
-    (set-face-font 'default "-apple-inconsolata-medium-r-normal--15-150-72-72-m-150-iso10646-1")))
-;; Free up the option key for special characters
-(setq ns-alternate-modifier (quote none))
-(setq ns-command-modifier (quote meta))
+    (set-face-font 'default "-apple-inconsolata-medium-r-normal--15-150-72-72-m-150-iso10646-1"))
+  ;; Free up the option key for special characters
+  (setq ns-alternate-modifier (quote none))
+  (setq ns-command-modifier (quote meta)))
 
 ;; Edit path crap
 (setenv "PATH" (concat "/Users/bradleyw/bin:/Users/bradleyw/Projects/homebrew/bin:"(getenv "PATH")))
