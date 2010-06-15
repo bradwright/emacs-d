bb5e3a37bf4acce2a4f6fc0e9ae901127e588f44
Bradley Wright <brad@intranation.com>
2010-06-15 08:57:08 +0100

    ido-menu has gone away

--- keys.el
+++ keys.el
@@ -9,9 +9,6 @@
 (global-set-key (kbd "C-M-s") 'isearch-forward)
 (global-set-key (kbd "C-M-r") 'isearch-backward)
 
-;; Jump to a definition in the current file. (This is awesome.)
-(global-set-key (kbd "C-x C-i") 'ido-imenu)
-
 ;; File finding
 (global-set-key (kbd "C-x M-f") 'ido-find-file-other-window)
 (global-set-key (kbd "C-x C-M-f") 'find-file-in-project)
