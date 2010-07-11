ff3a461a83210fe187093957bff05a8bb20719c1
Bradley Wright <brad@intranation.com>
2010-07-11 11:03:31 +0100

    Make Emacs behave a bit more like an OS X app on Darwin

--- darwin.el
+++ darwin.el
@@ -15,3 +15,32 @@
 ;; Edit path crap
 (setenv "PATH" (concat "/Users/bradleyw/bin:/Users/bradleyw/Projects/homebrew/bin:"(getenv "PATH")))
 (setq magit-git-executable "/Users/bradleyw/Projects/homebrew/bin/git")
+
+;; Make Emacs behave like a Mac app
+(global-set-key "\M-o" 'find-file)
+(global-set-key "\M-s" 'save-buffer)
+(global-set-key "\M-z" 'undo)
+(global-set-key [(meta down)] 'end-of-buffer)
+(global-set-key [(meta up)] 'beginning-of-buffer)
+(global-set-key [(meta right)] 'end-of-line)
+(global-set-key [(meta left)] 'beginning-of-line)
+
+;; can't seem to un-hijack cmd-`, so make it do something useful
+(global-set-key "\M-`" 'other-window-in-any-frame)
+
+;; switch to the next window, in any visible frame
+(defun other-window-in-any-frame (&optional arg)
+  "Switch to the next window using `next-window', with ALL-FRAMES
+set to 'visible.  If the next window is on a different frame
+switch to that frame first using `select-frame-set-input-focus'.
+
+If N is non-nil switch to the nth next window."
+  (interactive "p")
+  (while (> arg 0)
+    (let ((window (next-window (selected-window) nil 'visible)))
+      (when (not (member window (window-list)))
+        (dolist (frame (delq (selected-frame) (frame-list)))
+          (when (member window (window-list frame))
+            (select-frame-set-input-focus frame))))
+      (select-window window))
+    (decf arg)))
