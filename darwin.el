aca639fa4851c49e5160b690290de273a86bbe06
Bradley Wright <brad@intranation.com>
2010-07-11 11:16:27 +0100

    Make the app-like shortcuts only apply in GUI mode

--- darwin.el
+++ darwin.el
@@ -7,7 +7,18 @@
     (set-face-font 'default "-apple-inconsolata-medium-r-normal--15-150-72-72-m-150-iso10646-1"))
   ;; Free up the option key for special characters
   (setq ns-alternate-modifier (quote none))
-  (setq ns-command-modifier (quote meta)))
+  (setq ns-command-modifier (quote meta))
+  ;; Make Emacs behave like a Mac app
+  ;; Gotten from: http://github.com/samsonjs/config/blob/master/emacs
+  (global-set-key "\M-o" 'find-file)
+  (global-set-key "\M-s" 'save-buffer)
+  (global-set-key "\M-z" 'undo)
+  (global-set-key [(meta down)] 'end-of-buffer)
+  (global-set-key [(meta up)] 'beginning-of-buffer)
+  (global-set-key [(meta right)] 'end-of-line)
+  (global-set-key [(meta left)] 'beginning-of-line)
+  ;; can't seem to un-hijack cmd-`, so make it do something useful
+  (global-set-key "\M-`" 'other-window-in-any-frame))
 
 ;;; Ignore .DS_Store files with ido mode
 (add-to-list 'ido-ignore-files "\\.DS_Store")
@@ -15,19 +26,6 @@
 ;; Edit path crap
 (setenv "PATH" (concat "/Users/bradleyw/bin:/Users/bradleyw/Projects/homebrew/bin:"(getenv "PATH")))
 (setq magit-git-executable "/Users/bradleyw/Projects/homebrew/bin/git")
-
-;; Make Emacs behave like a Mac app
-;; Gotten from: http://github.com/samsonjs/config/blob/master/emacs
-(global-set-key "\M-o" 'find-file)
-(global-set-key "\M-s" 'save-buffer)
-(global-set-key "\M-z" 'undo)
-(global-set-key [(meta down)] 'end-of-buffer)
-(global-set-key [(meta up)] 'beginning-of-buffer)
-(global-set-key [(meta right)] 'end-of-line)
-(global-set-key [(meta left)] 'beginning-of-line)
-
-;; can't seem to un-hijack cmd-`, so make it do something useful
-(global-set-key "\M-`" 'other-window-in-any-frame)
 
 ;; switch to the next window, in any visible frame
 (defun other-window-in-any-frame (&optional arg)
