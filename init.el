5fba788416b9a959686fd3becddeeb0464427fb4
Bradley Wright <brad@intranation.com>
2010-10-09 14:32:14 +0100

    Configuration to allow editing as sudo on remote hosts

--- init.el
+++ init.el
@@ -154,6 +154,14 @@
 ;; start a server
 (server-start)
 
+;; Tramp mode: allow me to SSH to hosts and edit as sudo like:
+;;   C-x C-f /sudo:example.com:/etc/something-owned-by-root
+;; from: http://www.gnu.org/software/tramp/#Multi_002dhops
+(add-to-list 'tramp-default-proxies-alist
+             '(nil "\\`root\\'" "/ssh:%h:"))
+(add-to-list 'tramp-default-proxies-alist
+             '((regexp-quote (system-name)) nil nil))
+
 ;; Platform specific stuff
 ;; this is C/O: http://stackoverflow.com/questions/2548673/how-do-i-get-emacs-to-evaluate-a-file-when-a-frame-is-raised
 (add-hook 'after-make-frame-functions
