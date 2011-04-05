bd364393469de06b99efc47aaef1ac9f110dfc54
Bradley Wright <brad@intranation.com>
2011-04-05 14:21:49 +0100

    Add XML pretty printing functionality

--- elisp.el
+++ elisp.el
@@ -12,3 +12,19 @@
         (shell-command-to-string "$SHELL -l -c 'echo $PATH'"))))
     (setenv "PATH" path-from-shell)
     (setq exec-path (split-string path-from-shell path-separator))))
+
+;; gotten from: http://stackoverflow.com/questions/12492/pretty-printing-xml-files-on-emacs
+(defun bf-pretty-print-xml-region (begin end)
+  "Pretty format XML markup in region. You need to have nxml-mode
+http://www.emacswiki.org/cgi-bin/wiki/NxmlMode installed to do
+this.  The function inserts linebreaks to separate tags that have
+nothing but whitespace between them.  It then indents the markup
+by using nxml's indentation rules."
+  (interactive "r")
+  (save-excursion
+      (nxml-mode)
+      (goto-char begin)
+      (while (search-forward-regexp "\>[ \\t]*\<" nil t)
+        (backward-char) (insert "\n"))
+      (indent-region begin end))
+    (message "Ah, much better!"))
