2c201a32d41322eea63f29cb2017aeb0b812694c
Bradley Wright <brad@intranation.com>
2011-05-05 08:12:30 +0100

    Finish migration to *other* Solarized theme

--- gui.el
+++ gui.el
@@ -1,9 +1,12 @@
 ;; Color themes and graphical embellishment
 
 (require 'color-theme)
+;; general color themes
 (add-to-list 'load-path (concat dotfiles-dir "/vendor/themes"))
-(load "solarized")
-(color-theme-sanityinc-solarized-dark)
+;; Solarized theme with light/dark versions
+(add-to-list 'load-path (concat dotfiles-dir "/vendor/themes/color-theme-solarized"))
+(require 'color-theme-solarized)
+(color-theme-solarized-dark)
 
 (setq frame-title-format '(buffer-file-name "%f" ("%b")))
 (tooltip-mode -1)
