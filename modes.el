25820f71c2f94d9c0a70c20bbe82735a9d27007d
Bradley Wright <brad@intranation.com>
2011-11-12 11:03:17 +0000

    Force ansi-term to use utf-8 encoding

--- modes.el
+++ modes.el
@@ -130,3 +130,8 @@
   (setq erlang-man-root-dir (concat erlang-base-dir "/share/man"))
   (require 'erlang-start))
 
+;; ansi-term stuff
+;; force ansi-term to be utf-8 after it launches
+(defadvice ansi-term (after advise-ansi-term-coding-system)
+  (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))
+(ad-activate 'ansi-term)
