0b33706a2d82f4cb8ed7c4d700cb16092eddc84f
Bradley Wright <brad@intranation.com>
2012-01-08 21:24:42 +0000

    Revert part of "Massively simplify PATH hacking nonsense"
    This reverts commit d78c274893ad5c2a4a92deb9fd0cc919138a7eaf.

--- elisp.el
+++ elisp.el
@@ -7,8 +7,10 @@
 
 (defun set-exec-path-from-shell-PATH ()
   "Sets the exec-path to the same value used by the user shell"
-  (let (path-from-shell
-        (shell-command-to-string "echo -n $PATH"))
+  (let ((path-from-shell
+         (replace-regexp-in-string
+          "[[:space:]\n]*$" ""
+          (shell-command-to-string "$SHELL -l -c 'echo $PATH'"))))
     (setenv "PATH" path-from-shell)
     (setq exec-path (split-string path-from-shell path-separator))))
 
