af0e75cddfedda121b06fd8d642650026b535799
Bradley Wright <brad@intranation.com>
2012-03-20 16:39:29 +0000

    Make copy/paste work correctly in Terminal Emacs

--- darwin-cli.el
+++ darwin-cli.el
@@ -10,3 +10,18 @@
 ;; ... we want to be able to insert a '#' using Alt-3 in emacs as we would in other programs
 (fset 'insertPound "#")
 (define-key global-map "\M-3" 'insertPound)
+
+;; Make sure cut/paste works properly. Gotten from:
+;; http://mindlev.wordpress.com/2011/06/13/emacs-in-a-terminal-on-osx/#comment-20
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+;; Override defaults to use the mac copy and paste
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
