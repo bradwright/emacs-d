a76cffb5f02e0ddc7168ed171ba624f9c78f3516
Bradley Wright <brad@intranation.com>
2012-04-06 23:03:21 +0100

    Comments to make OS X CLI config more clear

--- darwin-cli.el
+++ darwin-cli.el
@@ -1,4 +1,7 @@
-;; nothing to see here
+;; Configuration to make Emacs run semi-normally in an OS X terminal
+
+;; XXX: strongly recommended to run in iTerm2, as it's more
+;; configurable than Terminal.app.
 
 ;; Enable the mouse, gotten from:
 ;; http://www.iterm2.com/#/section/faq
@@ -9,9 +12,12 @@
 ;; Make sure cut/paste works properly. Gotten from:
 ;; http://mindlev.wordpress.com/2011/06/13/emacs-in-a-terminal-on-osx/#comment-20
 (defun copy-from-osx ()
+  "Copies the current clipboard content using the `pbcopy`
+command"
   (shell-command-to-string "pbpaste"))
 
 (defun paste-to-osx (text &optional push)
+  "Copies the top of the kill ring stack to the OSX clipboard"
   (let ((process-connection-type nil))
     (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
       (process-send-string proc text)
