84c57397587851dad41c0d134952211585f47cbc
Bradley Wright <brad@intranation.com>
2012-04-07 22:29:03 +0100

    Reorganise, comment, and clean up key bindings

--- keys.el
+++ keys.el
@@ -4,16 +4,20 @@
 
 ;; You know, like Readline.
 (global-set-key (kbd "C-w") 'backward-kill-word)
+;; Since we've unset C-w, map it to something else
 (global-set-key (kbd "C-x C-k") 'kill-region)
+;; ... and the clumsy version
 (global-set-key (kbd "C-c C-k") 'kill-region)
 
 ;; Make regex searches easier to use
+
+;; these clash with paredit, so aren't available
 (global-set-key (kbd "M-s") 'isearch-forward-regexp)
 (global-set-key (kbd "M-r") 'isearch-backward-regexp)
 
 ;; File finding
 (global-set-key (kbd "C-x M-f") 'ido-find-file-other-window)
-(global-set-key (kbd "C-x C-M-f") 'find-file-in-project)
+(global-set-key (kbd "C-c C-f") 'find-file-in-project)
 (global-set-key (kbd "C-x f") 'recentf-ido-find-file)
 (global-set-key (kbd "C-x C-p") 'find-file-at-point)
 (global-set-key (kbd "C-c y") 'bury-buffer)
@@ -25,29 +29,23 @@
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-x t") 'bw-start-term)
 
-;; Window movement
-(global-set-key (kbd "C-x O") (lambda () (interactive) (other-window -1))) ;; back one
-(global-set-key (kbd "C-x C-o") (lambda () (interactive) (other-window 2))) ;; forward two
-
 ;; map M-x to C-x C-m and C-c C-m
 (global-set-key (kbd "C-x C-m") 'execute-extended-command)
 (global-set-key (kbd "C-c C-m") 'execute-extended-command)
-;; stupid GNUs
-(global-set-key (kbd "C-x m") 'execute-extended-command)
+;; Unset GNUs since it clashes with above and I don't use it
+(global-unset-key (kbd "C-x m"))
 
-;; on Mac this is Cmd+t, I don't use transpose words anyway
-(global-set-key (kbd "M-t") 'textmate-goto-file)
-
-;; flymake next error
+;; Navigation Flymake errors
 (global-set-key (kbd "C-c n") 'flymake-goto-next-error)
 (global-set-key (kbd "C-c p") 'flymake-goto-prev-error)
 
 ;; force myself to stop using M-x
-(global-set-key
- (kbd "M-x")
- (lambda ()
-   (interactive)
-   (message "Use C-x C-m!")))
+(defun bw-prevent-m-x ()
+  "Stop me from using M-x by warning me"
+  (interactive)
+  (message "Use C-x C-m!"))
+
+(global-set-key (kbd "M-x") 'bw-prevent-m-x)
 
 ;; anti-n00b mode
 (defun toggle-noob-mode ()
@@ -92,11 +90,13 @@
 (toggle-noob-mode)
 
 ;; prompt before killing emacs
-(global-set-key (kbd "C-x C-c")
-  (function
-   (lambda () (interactive)
-     (cond ((y-or-n-p "Quit? ")
-       (save-buffers-kill-emacs))))))
+(defun bw-kill-emacs ()
+  "Warn before exiting Emacs"
+  (interactive)
+  (cond ((y-or-n-p "Quit Emacs? ")
+         (save-buffers-kill-emacs))))
+
+(global-set-key (kbd "C-x C-c") 'bw-kill-emacs)
 
 ;; Org-mode
 (global-set-key (kbd "C-M-r") 'org-capture)
