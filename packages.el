e0049f90c54fc4d5fd83dc13520c0553adef1e49
Bradley Wright <brad@intranation.com>
2012-04-09 14:24:46 +0100

    Add MELPA to package lists

--- packages.el
+++ packages.el
@@ -10,6 +10,7 @@
 (setq package-user-dir (concat dotfiles-dir "/.elpa"))
 
 (add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/")
              '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 (package-initialize)
