00ea25af1a7b5aa93360c5196b9c4b4a27503de2
Bradley Wright <brad@intranation.com>
2012-05-06 21:50:00 +0100

    Make heading for recentf shorter

--- elisp.el
+++ elisp.el
@@ -1,7 +1,7 @@
 (defun recentf-ido-find-file ()
   "Find a recent file using ido."
   (interactive)
-  (let ((file (ido-completing-read "Choose recent file: " recentf-list nil t)))
+  (let ((file (ido-completing-read "Recently: " recentf-list nil t)))
     (when file
       (find-file file))))
 
