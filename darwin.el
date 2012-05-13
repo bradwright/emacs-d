55c9f6c1fea234891a7c2e941ce936d614fb43e3
Bradley Wright <brad@intranation.com>
2012-05-13 22:16:41 +0100

    Indentation

--- darwin.el
+++ darwin.el
@@ -31,9 +31,9 @@
   (load-theme 'solarized-dark t))
 
 (when (or
-     (display-graphic-p)
-     (search "Solarized" (getenv "ITERM_PROFILE")))
-    (add-hook 'bw-after-custom-load-hook 'load-solarized-dark-theme))
+       (display-graphic-p)
+       (search "Solarized" (getenv "ITERM_PROFILE")))
+  (add-hook 'bw-after-custom-load-hook 'load-solarized-dark-theme))
 
 ;; Even though we may have set the Mac OS X Terminal's Alt key as the
 ;; emacs Meta key ...
