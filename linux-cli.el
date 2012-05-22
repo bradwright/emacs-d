fdf9757acd01f95059d82870b5d308edcdd04865
Bradley Wright <brad@intranation.com>
2012-05-22 19:06:31 +0100

    Report xterm mouse in Linux as well

--- linux-cli.el
+++ linux-cli.el
@@ -1,3 +1,9 @@
 ;;; Linux CLI changes
 
 (color-theme-solarized-dark)
+
+;; Enable the mouse, gotten from:
+;; http://www.iterm2.com/#/section/faq
+(require 'mouse)
+(xterm-mouse-mode t)
+(defun track-mouse (e))
