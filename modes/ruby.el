c422f5773b081bb70f081b14565167128da1faa2
Bradley Wright <brad@intranation.com>
2012-06-12 08:32:13 +0100

    Include Rakefiles in Ruby mode

--- modes/ruby.el
+++ modes/ruby.el
@@ -9,6 +9,7 @@
 
 (add-to-list 'auto-mode-alist '("[vV]agrantfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 
 (add-to-list 'auto-mode-alist '("\\.css\\.erb$" . css-mode))
 
