d9ab39cd66f091d8b803bdc73777d5c4aa80e6e5
Bradley Wright <brad@intranation.com>
2012-06-15 09:36:52 +0100

    Don't save TRAMP files to desktop

--- modes.el
+++ modes.el
@@ -159,7 +159,6 @@
       desktop-base-lock-name      "lock"
       desktop-path                (list desktop-dirname)
       desktop-save                t
-      desktop-files-not-to-save   "^$" ;reload tramp paths
       desktop-load-locked-desktop nil)
 (desktop-save-mode 1)
 
