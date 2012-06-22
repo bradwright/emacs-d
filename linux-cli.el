e39f39a392f6dfea052e6a94208015aa4fbcdbf8
Bradley Wright <brad@intranation.com>
2012-06-22 22:03:23 +0100

    Don't actually enable the mouse

--- linux-cli.el
+++ linux-cli.el
@@ -7,9 +7,3 @@
     (load-theme 'solarized-dark t)
   (when (fboundp 'color-theme)
     (color-theme-solarized-dark)))
-
-;; Enable the mouse, gotten from:
-;; http://www.iterm2.com/#/section/faq
-(require 'mouse)
-(xterm-mouse-mode t)
-(defun track-mouse (e))
