5d397e6bd90d4d24b268de8c24ef39b715ef49b3
Bradley Wright <brad@intranation.com>
2012-06-23 10:49:46 +0100

    Use tango-dark on CLI

--- linux-cli.el
+++ linux-cli.el
@@ -4,6 +4,4 @@
 
 ;; Conditional colour theme loading
 (if (eq emacs-major-version 24)
-    (load-theme 'solarized-dark t)
-  (when (fboundp 'color-theme)
-    (color-theme-solarized-dark)))
+    (load-theme 'tango-dark t))
