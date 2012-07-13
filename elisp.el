f64d44cf9904d8781e82cb171e60fa5f044721f7
Bradley Wright <brad@intranation.com>
2012-07-13 06:50:52 +0100

    Saving IDO find tag

--- elisp.el
+++ elisp.el
@@ -171,3 +171,14 @@ environment."
   "Loads all files resident in the `modes` directory"
   (let ((modes-dir (or load-from (concat dotfiles-dir "modes"))))
     (mapc 'load (directory-files modes-dir t "^[^#].*el$"))))
+
+(defun bw-ido-find-tag ()
+  "Find a tag using ido"
+  (interactive)
+  (tags-completion-table)
+  (let (tag-names)
+    (mapc (lambda (x)
+            (unless (integerp x)
+              (push (prin1-to-string x t) tag-names)))
+          tags-completion-table)
+    (find-tag (ido-completing-read "Tag: " tag-names))))
