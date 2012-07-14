ca638c2096761faf06a7c56ef6686a3a307b9bd7
Bradley Wright <brad@intranation.com>
2012-07-14 22:40:24 +0100

    Clean up Rails config

--- modes/ruby.el
+++ modes/ruby.el
@@ -2,9 +2,6 @@
 
 ;; Ruby mode
 
-;; this variable is stupid - apparently Ruby needs its own indent
-;; variable
-;; 2-space indent is idiomatic
 (use-package ruby-mode
   :mode (("\\.rb\\'" . ruby-mode)
          ("[vV]agrantfile$" . ruby-mode)
@@ -23,48 +20,8 @@
 
     (add-hook 'ruby-on-rails-project-file-visit-hook 'eproject-rails-config)
 
-    ;; TODO: can we factor this out into a macro?
-    (defun find-rails-type (predicate title)
-      "Finds a file filtered by predicate"
-      (when (eq (eproject-type) 'ruby-on-rails)
-        (find-file
-         (concat
-          (eproject-root)
-          (ido-completing-read
-           title
-           (mapcar
-            (lambda (e)
-              (replace-regexp-in-string (eproject-root) "" e))
-            (remove-if-not predicate (eproject-list-project-files))))))))
-
-    (defun find-controllers-rails ()
-      "Finds controller files in an eproject-rails project"
-      (interactive)
-      (find-rails-type 'is-controller "Controller: "))
-
-    (defun find-models-rails ()
-      "Finds model files in an eproject-rails project"
-      (interactive)
-      (find-rails-type 'is-model "Model: "))
-
-    (defun find-views-rails ()
-      "Finds view files in an eproject-rails project"
-      (interactive)
-      (find-rails-type 'is-view "View: "))
-
-    (defun is-controller (name)
-      "Filters a string"
-      (interactive "sController: ")
-      (not (equal (string-match "app/controller*" name) nil)))
-
-    (defun is-model (name)
-      (interactive "sModel: ")
-      (not (equal (string-match "app/models*" name) nil)))
-
-    (defun is-view (name)
-      (interactive "sView: ")
-      (not (equal (string-match "app/views*" name) nil)))
-
+    ;; this variable is stupid - apparently Ruby needs its own indent
+    ;; variable. 2-space indent is idiomatic
     (setq ruby-indent-level 2)))
 
 (add-to-list 'auto-mode-alist '("\\.css\\.erb$" . css-mode))
