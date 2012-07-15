a767ae9a5ff5472ee887c50f59be0bd4bb8fb88c
Bradley Wright <brad@intranation.com>
2012-07-15 21:22:17 +0100

    Refactor packages.el into use-package style

--- packages.el
+++ packages.el
@@ -1,52 +1,54 @@
 ;;; -*- lexical-binding: t -*-
 
 ;; packages.el - manages packages and lists of packages
-(require 'package)
-;; override my package directory
-(setq package-user-dir (file-name-as-directory (concat dotfiles-dir ".elpa/")))
-(make-directory package-user-dir t)
+(use-package package
+  :init
+  (progn
+    ;; override my package directory
+    (setq package-user-dir (file-name-as-directory (concat dotfiles-dir ".elpa/")))
+    (make-directory package-user-dir t)
 
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+    (add-to-list 'package-archives
+                 '("marmalade" . "http://marmalade-repo.org/packages/") t)
+    (add-to-list 'package-archives
+                 '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
-(package-initialize)
+    (package-initialize)
 
-;; Only some packages we want bleeding edge
-(setq package-archive-exclude-alist
-      '(("melpa"
-         clojure-mode
-         slime ;; slime is attached to clojure-mode
-         clojure-test-mode
-         haskell-mode
-         idomenu
-         znc)))
+    ;; Only some packages we want bleeding edge
+    (setq package-archive-exclude-alist
+          '(("melpa"
+             clojure-mode
+             slime ;; slime is attached to clojure-mode
+             clojure-test-mode
+             haskell-mode
+             idomenu
+             znc)))
 
-;; refresh archives
-(when (null package-archive-contents)
-  (package-refresh-contents))
+    ;; refresh archives
+    (when (null package-archive-contents)
+      (package-refresh-contents))
 
-;; install melpa first so we can filter packages
-(when (not (package-installed-p 'melpa))
-  (package-install 'melpa)
-  (package-refresh-contents))
+    ;; install melpa first so we can filter packages
+    (when (not (package-installed-p 'melpa))
+      (package-install 'melpa)
+      (package-refresh-contents))
 
-;; packages I install
-(defvar packages-to-install
-  '(git-commit
-    paredit
-    yaml-mode
-    scss-mode
-    python-mode
-    mustache-mode
-    flymake-cursor
-    markdown-mode
-    puppet-mode
-    idomenu
-    ack-and-a-half))
+    ;; packages I install
+    (defvar packages-to-install
+      '(git-commit
+        paredit
+        yaml-mode
+        scss-mode
+        python-mode
+        mustache-mode
+        flymake-cursor
+        markdown-mode
+        puppet-mode
+        idomenu
+        ack-and-a-half))
 
-;; install everything in that list
-(dolist (p packages-to-install)
-  (when (not (package-installed-p p))
-    (package-install p)))
+    ;; install everything in that list
+    (dolist (p packages-to-install)
+      (when (not (package-installed-p p))
+        (package-install p)))))
