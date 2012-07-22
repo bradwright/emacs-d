e4a1ab6ac8b84cda2e7251e8e35b1ad3950b16aa
Bradley Wright <brad@intranation.com>
2012-07-22 21:14:58 +0100

    Move some things into a "if gui" check

--- gui.el
+++ gui.el
@@ -2,13 +2,6 @@
 
 ;; Color themes and graphical embellishment
 
-;; show lines after end of buffer
-(setq indicate-empty-lines t)
-
-(defvar bw-after-frame-raise-hook '()
-  "Hook called after a frame is raised. Functions receive a
-  single arg, which is the frame that was raised.")
-
 ;; Theme files
 (setq theme-dotfiles-dir (file-name-as-directory (concat dotfiles-dir "vendor/themes/")))
 (make-directory theme-dotfiles-dir t)
@@ -28,22 +21,24 @@
 
 (load-custom-themes)
 
-;; From:
-;; http://emacs-fu.blogspot.co.uk/2011/01/setting-frame-title.html
-(setq frame-title-format
-      '("emacs@" (:eval (system-name)) ": "
-        (:eval (if (buffer-file-name)
-                   (abbreviate-file-name (buffer-file-name))
-                 "%b"))))
-
 (when (display-graphic-p)
+  ;; From:
+  ;; http://emacs-fu.blogspot.co.uk/2011/01/setting-frame-title.html
+  (setq frame-title-format
+        '("emacs@" (:eval (system-name)) ": "
+          (:eval (if (buffer-file-name)
+                     (abbreviate-file-name (buffer-file-name))
+                   "%b"))))
+
   ;; show help in the echo area instead of as a tooltip
   (tooltip-mode -1)
   ;; make fringe-mode 4 pixels
   (fringe-mode 4)
   ;; blink the cursor
   (setq blink-cursor-interval 1.0)
-  (blink-cursor-mode))
+  (blink-cursor-mode)
+  ;; show lines after end of buffer
+  (setq indicate-empty-lines t))
 
 ;; stop beeping at me
 (setq visible-bell t)
