585a0157eb875008d14f2d40b7443c1bf411632f
Bradley Wright <brad@intranation.com>
2012-08-26 19:17:01 +0100

    Conditionally load Solarized Dark in the terminal
    If the terminal supports 256 colours or more, we know that
    solarized-dark will display correctly, so we can load it.

--- linux-cli.el
+++ linux-cli.el
@@ -1,3 +1,7 @@
 ;;; -*- lexical-binding: t -*-
 
 ;;; Linux CLI changes
+
+;; if the terminal supports 256 or more, load Solarized Dark
+(if (>= (display-color-cells) 256)
+    (load-solarized-theme))
