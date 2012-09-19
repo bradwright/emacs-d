5b6a38a10ab50a6bedce07690e51198f99c7f5d2
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2012-09-19 13:03:39 +0100

    Load local overrides automatically

--- init.el
+++ init.el
@@ -127,6 +127,22 @@
   (unless (display-graphic-p)
     (load "linux-cli")))
 
+;; load local files
+;; local overrides
+(setq local-dotfiles-dir (bw-join-dirs dotfiles-dir "local"))
+
+(setq
+ bw-user-config (concat local-dotfiles-dir user-login-name ".el")
+ bw-system-config (concat local-dotfiles-dir system-name ".el"))
+
+(defun load-local-configs ()
+  (when (file-exists-p bw-user-config)
+    (load bw-user-config))
+  (when (file-exists-p bw-system-config)
+    (load bw-system-config)))
+
+(add-hook 'after-init-hook 'load-local-configs)
+
 ;; Load custom file last
 (setq custom-file (concat dotfiles-dir "custom.el"))
 (load custom-file 'noerror)
