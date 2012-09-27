3fe517b527f42c492b6f90ec5a2ecfcd5977c57b
Bradley Wright <brad@intranation.com>
2012-09-27 22:46:25 +0100

    Clean up Ruby mode a bit
    - Don't deep indent inside parens
    - Fix custom IRB prompt for inf-ruby-mode
    - Fix subword-mode for Ruby classes
    - Kill Rinari customisations

--- modes/ruby.el
+++ modes/ruby.el
@@ -24,10 +24,15 @@
     (add-hook 'ruby-on-rails-project-file-visit-hook 'eproject-rails-config)
 
     ;; Ruby has a lot of camel case
-    (subword-mode)
+    (add-hook 'ruby-mode-hook (lambda ()
+                                (subword-mode)))
 
     ;; this variable is stupid - apparently Ruby needs its own indent
-    (setq ruby-indent-level 2))
+    (setq ruby-indent-level 2
+          ;; don't deep indent parens
+          ruby-deep-indent-paren nil
+          ;; don't insert an encoding comment automatically
+          ruby-insert-encoding-magic-comment nil))
 
   ;; fix syntax highlighting for Cucumber Step Definition regexps
   (add-to-list 'ruby-font-lock-syntactic-keywords
@@ -39,8 +44,11 @@
 (add-to-list 'auto-mode-alist '("\\.scss\\.erb$" . scss-mode))
 (add-to-list 'auto-mode-alist '("\\.json\\.erb$" . json-mode))
 
-;; add a bundle version of IRB shell
-(use-package rinari
+(use-package inf-ruby
   :config
   (progn
-    (add-to-list 'inf-ruby-implementations '("bundle-ruby" . "bundle exec irb --inf-ruby-mode -r irb/completion"))))
+    ;; bundle compatible inf-ruby
+    (add-to-list 'inf-ruby-implementations '("bundle-ruby" . "bundle exec irb --inf-ruby-mode -r irb/completion"))
+
+    (setq inf-ruby-first-prompt-pattern "^>> $"
+          inf-ruby-prompt-pattern       "^>> $")))
