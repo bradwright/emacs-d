2b4c31ec9691d52ed9fd3f3cc01fe1b45391718a
Bradley Wright <brad@intranation.com>
2012-10-04 23:47:36 +0100

    Refactor packages
    Inspired by some cool stuff in Emacs Prelude, I've refactored my
    packages file to be more organised and load less by default.

--- packages.el
+++ packages.el
@@ -4,27 +4,66 @@
 (use-package package
   :init
   (progn
-    ;;------------------------------------------------------------------------------
+    ;; override my package directory
+    (setq package-user-dir
+          (file-name-as-directory (concat dotfiles-dir ".elpa/")))
+    (make-directory package-user-dir t)
+
+    ;; I use Marmalade and Melpa
+    (add-to-list 'package-archives
+                 '("marmalade" . "http://marmalade-repo.org/packages/") t)
+    (add-to-list 'package-archives
+                 '("melpa" . "http://melpa.milkbox.net/packages/") t)
+    (package-initialize)
+
+    ;; stop keepalives causing weird HTTP errors
+    (setq url-http-attempt-keepalives nil)
+
+    ;; This makes heavy use of Melpa, so make sure it's installed
+    (when (not (package-installed-p 'melpa))
+      (progn
+        (switch-to-buffer
+         (url-retrieve-synchronously
+          "https://raw.github.com/milkypostman/melpa/master/melpa.el"))
+        (package-install-from-buffer (package-buffer-info) 'single)))
+
+    ;;-------------------------------------------------------------------------
     ;; Patch up annoying package.el quirks
-    ;;------------------------------------------------------------------------------
+    ;;-------------------------------------------------------------------------
     ;; Gotten from: https://github.com/purcell/emacs.d/blob/master/init-elpa.el
 
-    (defadvice package-generate-autoloads (after close-autoloads (name pkg-dir) activate)
+    (defadvice package-generate-autoloads
+      (after close-autoloads (name pkg-dir) activate)
       "Stop package.el from leaving open autoload files lying around."
       (let ((path (expand-file-name (concat name "-autoloads.el") pkg-dir)))
         (with-current-buffer (find-file-existing path)
           (kill-buffer nil))))
 
-    ;; override my package directory
-    (setq package-user-dir (file-name-as-directory (concat dotfiles-dir ".elpa/")))
-    (make-directory package-user-dir t)
+    ;; essential packages - I always install these
+    (defvar essential-packages
+      '(ack-and-a-half
+        diminish
+        expand-region
+        flymake-cursor
+        idomenu
+        magithub
+        multiple-cursors
+        paredit))
 
-    (add-to-list 'package-archives
-                 '("marmalade" . "http://marmalade-repo.org/packages/") t)
-    (add-to-list 'package-archives
-                 '("melpa" . "http://melpa.milkbox.net/packages/") t)
+    (defun essential-packages-installed-p ()
+      (loop for p in essential-packages
+            when (not (package-installed-p p)) do (return nil)
+            finally (return t)))
 
-    (package-initialize)
+    (defun install-essential-packages ()
+      (unless (essential-packages-installed-p)
+        (message "%s" "Installing essential packages...")
+        (package-refresh-contents)
+        (dolist (p essential-packages)
+          (unless (package-installed-p p)
+            (package-install p)))))
+
+    (install-essential-packages)
 
     ;; Only some packages we want bleeding edge
     (setq package-archive-exclude-alist
@@ -35,37 +74,4 @@
              haskell-mode
              idomenu
              ruby-mode
-             znc)))
-
-    ;; refresh archives
-    (when (null package-archive-contents)
-      (package-refresh-contents))
-
-    ;; install melpa first so we can filter packages
-    (when (not (package-installed-p 'melpa))
-      (package-install 'melpa)
-      (package-refresh-contents))
-
-    ;; packages I install
-    (defvar packages-to-install
-      '(paredit
-        diminish
-        ace-jump-mode
-        yaml-mode
-        scss-mode
-        python-mode
-        mustache-mode
-        flymake-cursor
-        markdown-mode
-        puppet-mode
-        idomenu
-        ack-and-a-half
-        inf-ruby
-        expand-region
-        multiple-cursors
-        browse-kill-ring))
-
-    ;; install everything in that list
-    (dolist (p packages-to-install)
-      (when (not (package-installed-p p))
-        (package-install p)))))
+             znc)))))
