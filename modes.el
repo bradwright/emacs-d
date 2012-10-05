03672f66e0294e5e087dd083921ebc5bb1cb3b38
Bradley Wright <brad@intranation.com>
2012-10-05 22:37:34 +0100

    Move eproject config to a separate file
    Also force eproject-extras to load.

--- modes.el
+++ modes.el
@@ -188,56 +188,6 @@
           ediff-diff-options          "-w"
           ediff-window-setup-function 'ediff-setup-windows-plain)))
 
-;; eproject mode
-(use-package eproject
-  :diminish eproject-mode
-  :config
-  (progn
-    ;; ruby on rails special type
-    (define-project-type ruby-on-rails-git (generic-git)
-      (and (look-for ".git")
-           (look-for "Gemfile")
-           (look-for "config/application.rb"))
-      :main-file "Gemfile")
-
-    (defun rails-eproject-test (&optional only-this-file)
-      "Runs local tests"
-      (interactive "P")
-      (if (eq only-this-file nil)
-          (compile "bundle exec rake" t)
-        (compile (concat "bundle exec ruby -I test " (buffer-file-name)) t)))
-
-    (defun rails-eproject-hook ()
-      "Set up some local variables"
-      (put 'scss-sass-command 'safe-local-variable 'stringp)
-
-      ;; (shell-command "git ls-files | /usr/local/bin/ctags -e -f - -L - 1> TAGS 2> /dev/null")
-
-      (set (make-local-variable 'inf-ruby-default-implementation) "bundle-ruby")
-
-      ;; run rake to compile
-      (set (make-local-variable 'compile-command) "bundle exec rake")
-      (local-set-key (kbd "C-c C-t") 'rails-eproject-test))
-
-    (add-hook 'ruby-on-rails-git-project-file-visit-hook 'rails-eproject-hook)
-
-    (defun bw-eproject-find-files ()
-      "If we're in a Git project, use git ls-files to look up the
-files, because it won't try to open any .gitignored files."
-      (interactive)
-      (if (member (eproject-type) '(generic-git ruby-on-rails-git))
-          (let ((default-directory (eproject-root)))
-            (find-file
-             (concat
-              (eproject-root)
-              (ido-completing-read
-               (format "Find file: %s" (bw-shorten-dir (eproject-root)))
-               (split-string (shell-command-to-string "git ls-files --exclude-standard -co"))))))
-        (eproject-find-file)))
-    (use-package eproject-extras
-      :bind ("C-c f" . bw-eproject-find-files))
-    (setq eproject-completing-read-function 'eproject--ido-completing-read)))
-
 ;; coffee script and iced coffee
 (use-package coffee-mode
   :mode (("\\.coffee\\'" . coffee-mode)
