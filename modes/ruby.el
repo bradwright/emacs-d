86a89871b459e6153a8187a28431bb05b6ee4aa7
Bradley Wright <brad@intranation.com>
2012-10-12 22:34:04 +0100

    Ignore .bundle and log directories

--- modes/ruby.el
+++ modes/ruby.el
@@ -19,7 +19,12 @@
 
       ;; We don't want to compile SCSS in Rails because the asset pipeline
       ;; does it for us
-      (set (make-local-variable 'scss-compile-at-save) nil))
+      (set (make-local-variable 'scss-compile-at-save) nil)
+
+      (set (make-local-variable 'grep-find-ignored-directories) grep-find-ignored-directories)
+      (add-to-list 'grep-find-ignored-directories "log"))
+
+    (add-to-list 'grep-find-ignored-directories ".bundle")
 
     (add-hook 'ruby-on-rails-project-file-visit-hook 'eproject-rails-config)
 
