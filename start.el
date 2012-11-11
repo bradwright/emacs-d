d385a9d5b8700598568c70ca3e520bcd2460875b
Bradley Wright <brad@intranation.com>
2012-11-11 08:08:28 +0800

    Port IDO functionality

--- start.el
+++ start.el
@@ -138,6 +138,24 @@
   (progn
     (message "Loading Linux config")))
 
+;;; Modes
+
+;; IDO mode
+(ido-mode t)
+(ido-everywhere t)
+
+(setq
+ ;; Match arbitrary points in strings
+ ido-enable-prefix nil
+ ;; Match across entire string
+ ido-enable-flex-matching t
+ ;; Create a new buffer if there's no match candidate
+ ido-create-new-buffer 'always
+ ;; Don't try and guess if the string under point is a file
+ ido-use-filename-at-point nil
+ ;; case-insensitive matching
+ ido-case-fold t)
+
 ;;; Global keyboard combinations
 
 ;; map M-x to C-x C-m and C-c C-m, because M-x is in an awkward spot
