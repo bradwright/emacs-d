10db78f72ca680c36d4118ce42d878021af2e3fd
Bradley Wright <brad@intranation.com>
2012-11-11 21:56:07 +0800

    Add kill-ring/region advice

--- init-editing.el
+++ init-editing.el
@@ -31,4 +31,21 @@
 ;; nuke trailing whitespace when writing to a file
 (add-hook 'write-file-hooks 'delete-trailing-whitespace)
 
+;; http://emacs-fu.blogspot.hk/2009/11/copying-lines-without-selecting-them.html
+(defadvice kill-ring-save (before slick-copy activate compile)
+  "When called interactively with no active region, copy a single line instead."
+  (interactive
+   (if mark-active (list (region-beginning) (region-end))
+     (message "Copied line")
+     (list (line-beginning-position)
+           (line-beginning-position
+            2)))))
+
+(defadvice kill-region (before slick-cut activate compile)
+  "When called interactively with no active region, kill a single line instead."
+  (interactive
+    (if mark-active (list (region-beginning) (region-end))
+      (list (line-beginning-position)
+            (line-beginning-position 2)))))
+
 (provide 'init-editing)
