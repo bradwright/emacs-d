8b562dd51d7f13d8b37d9013e639a3e8607dc2ab
Bradley Wright <brad@intranation.com>
2012-11-11 22:25:32 +0800

    Move keybindings to their own file

--- init.el
+++ init.el
@@ -76,28 +76,7 @@
 (require 'init-themes)
 (require 'init-solarized)
 
-;;; Global keyboard combinations
-
-;; map M-x to C-x C-m and C-c C-m, because M-x is in an awkward spot
-(global-set-key (kbd "C-x C-m") 'execute-extended-command)
-(global-set-key (kbd "C-c C-m") 'execute-extended-command)
-;; Unset GNUs since it clashes with above and I don't use it
-(global-unset-key (kbd "C-x m"))
-;; unset M-x due to above
-(global-unset-key (kbd "M-x"))
-
-;; Use hippie-expand instead of dabbrev
-(global-set-key (kbd "M-/") 'hippie-expand)
-
-;; Copy readline's kill word
-(global-set-key (kbd "C-w") 'backward-kill-word)
-;; Since we've unset C-w, map it to something else
-(global-set-key (kbd "C-x C-k") 'kill-region)
-;; ... and the clumsy version
-(global-set-key (kbd "C-c C-k") 'kill-region)
-
-(global-set-key (kbd "C-c y") 'bury-buffer)
-(global-set-key (kbd "C-c r") 'revert-buffer)
+(require 'init-keybindings)
 
 ;; start a server, unless one is already running
 (require 'server)
