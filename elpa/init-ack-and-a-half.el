5923af5a2869ebfde7b51668d774633c6a608479
Bradley Wright <brad@intranation.com>
2012-11-12 23:25:09 +0800

    Tidy up ack-and-a-half arguments

--- elpa/init-ack-and-a-half.el
+++ elpa/init-ack-and-a-half.el
@@ -1,16 +1,20 @@
 (use-package ack-and-a-half
   :init
   (progn
-    (setq ack-and-a-half-regexp-search nil)
-    (put 'ack-and-a-half-arguments 'safe-local-variable 'listp))
+    (setq
+     ;; don't use regexes by default
+     ack-and-a-half-regexp-search nil
+     ;; my ackrc has a pager by default, so clobber it
+     ack-and-a-half-arguments '("--nopager")
+     ;; prompt for a directory instead of just accepting project root
+     ack-and-a-half-prompt-for-directory t))
   :bind ("C-c C-a" . ack-and-a-half)
   :config
   (progn
-    (add-to-list 'ack-and-a-half-arguments "--nopager")
-    (setq ack-and-a-half-prompt-for-directory t)
+
     (add-hook 'eproject-first-buffer-hook (lambda ()
                                             (when (eproject-root)
-      (set (make-local-variable 'ack-and-a-half-root-directory-functions) 'ack-and-a-half-root-directory-functions)
-      (add-to-list 'ack-and-a-half-root-directory-functions 'eproject-root))))))
+                                              (set (make-local-variable 'ack-and-a-half-root-directory-functions) 'ack-and-a-half-root-directory-functions)
+                                              (add-to-list 'ack-and-a-half-root-directory-functions 'eproject-root))))))
 
 (provide 'init-ack-and-a-half)
