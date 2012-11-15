aec521440fd3f60440321dd23a66d05efd23e8e9
Bradley Wright <brad@intranation.com>
2012-11-15 17:01:37 +0800

    Lazy load ack-and-a-half arguments in project

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,4 @@
-((nil . ((eval . (add-to-list 'ack-and-a-half-arguments "--ignore-dir=elpa/installed"))
-         (eval . (add-to-list 'ack-and-a-half-arguments "--ignore-dir=vendor")))))
+((nil . ((eval . (eval-after-load 'ack-and-a-half
+                   '(progn
+                      (add-to-list 'ack-and-a-half-arguments "--ignore-dir=elpa/installed")
+                      (add-to-list 'ack-and-a-half-arguments "--ignore-dir=vendor")))))))
