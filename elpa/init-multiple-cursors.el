476fa856cd283f54684ea6bcad8b46224a598919
Bradley Wright <brad@intranation.com>
2012-11-24 11:17:13 +0000

    More keybindings for multiple-cursors

--- elpa/init-multiple-cursors.el
+++ elpa/init-multiple-cursors.el
@@ -1,6 +1,8 @@
 (use-package multiple-cursors
   :bind (("C-c ." . mc/mark-next-like-this)
+         ("C->" . mc/mark-next-like-this)
          ("C-c ," . mc/mark-previous-like-this)
+         ("C-<" . mc/mark-previous-like-this)
          ("C-c C-l". mc/mark-all-like-this)))
 
 (provide 'init-multiple-cursors)
