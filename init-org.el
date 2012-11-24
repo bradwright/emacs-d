4de19ca0f60c15a49364abb468c91127106e9ee6
Bradley Wright <brad@intranation.com>
2012-11-24 11:08:42 +0000

    Change org-mode configuration to load async

--- init-org.el
+++ init-org.el
@@ -1,19 +1,23 @@
-;; Show other programming languages semi-natively when embedded
-(org-babel-do-load-languages
- 'org-babel-load-languages
- ;; load emacs-lisp natively
- '((emacs-lisp . t)
-   (ruby . t)))
+(use-package org
+  :config
+  (progn
+    ;; auto-wrap
+    (add-hook 'org-mode-hook 'bw-turn-on-auto-fill)
+    (add-hook 'org-mode-hook 'bw-turn-off-trailing-whitespace)
 
-;; edit inline code blocks natively
-(setq org-src-fontify-natively t)
-(setq org-src-tab-acts-natively t)
+    ;; edit inline code blocks natively
+    (setq
+     org-src-fontify-natively t
+     org-src-tab-acts-natively t)
 
-;; auto-wrap
-(add-hook 'org-mode-hook 'bw-turn-on-auto-fill)
-(add-hook 'org-mode-hook 'bw-turn-off-trailing-whitespace)
+    ;; Show other programming languages semi-natively when embedded
+    (org-babel-do-load-languages
+     'org-babel-load-languages
+     ;; load emacs-lisp natively
+     '((emacs-lisp . t)
+       (ruby . t)))
 
-;; default directory
-;;(setq org-directory "~/Dropbox/Org/")
+    ;; default directory
+    (setq org-directory "~/Dropbox/Org/")))
 
 (provide 'init-org)
