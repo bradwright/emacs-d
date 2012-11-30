75389733cb4741cbf56ddb1b889eeb18ebaa24c2
Bradley Wright <brad@intranation.com>
2012-11-30 22:14:36 +0000

    Turn on eldoc-mode when editing Emacs lisp

--- init-emacs-lisp.el
+++ init-emacs-lisp.el
@@ -1,3 +1,5 @@
 (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
+;; eldoc mode provides documentation in the minibuffer automatically
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 
 (provide 'init-emacs-lisp)
