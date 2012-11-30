bb3516908f02f5371644310f16f5455b74ad2214
Bradley Wright <brad@intranation.com>
2012-11-30 23:04:56 +0000

    Diminish eldoc-mode

--- modes/init-emacs-lisp.el
+++ modes/init-emacs-lisp.el
@@ -3,4 +3,7 @@
 ;; eldoc mode provides documentation in the minibuffer automatically
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 
+(use-package eldoc
+  :diminish eldoc-mode)
+
 (provide 'init-emacs-lisp)
