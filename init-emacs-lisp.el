de0ba619dd56af40877209888f3fa02dbbaccde8
Bradley Wright <brad@intranation.com>
2012-11-30 22:14:51 +0000

    Documentation for paredit-mode in Emacs lisp

--- init-emacs-lisp.el
+++ init-emacs-lisp.el
@@ -1,3 +1,4 @@
+;; paredit mode autopairs sexps and provides tools to manipulate sexps
 (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
 ;; eldoc mode provides documentation in the minibuffer automatically
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
