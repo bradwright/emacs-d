bd026265ce2cfb8188cb184a8c5bec8e85bf85c9
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2012-12-03 11:40:30 +0000

    Magic commands to copy/paste from remote machine
    See corresponding changes in bradleywright/dotfiles

--- init-linux.el
+++ init-linux.el
@@ -1,5 +1,18 @@
 ;;; Linux stuff
 (when *is-linux*
-  (progn))
+  (progn
+
+    ;; remote copy/paste from host OS X machine
+    (defun copy-from-remote ()
+      (shell-command-to-string "~/bin/rpbpaste"))
+
+    (defun paste-to-remote (text &optional push)
+      "Copies the top of the kill ring stack to the OSX clipboard"
+      (let ((process-connection-type nil))
+        (let ((proc (start-process "rpbcopy" "*Messages*" "~/bin/rpbcopy")))
+          (process-send-string proc text)
+          (process-send-eof proc))))
+    (setq interprogram-paste-function 'copy-from-remote)
+    (setq interprogram-cut-function 'paste-to-remote)))
 
 (provide 'init-linux)
