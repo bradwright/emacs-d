4bf137d71c460b5d91ba7964619c5f5773a9bcbd
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2012-12-17 12:32:28 +0000

    vendor doesn't exist any more

--- init-paths.el
+++ init-paths.el
@@ -1,26 +1,11 @@
 ;;; load path stuff
 
-;; external libraries I might have collected via submodules etc.
-(defconst vendor-dotfiles-dir
-  (bw-join-dirs dotfiles-dir "vendor")
-  "Vendorised Emacs libraries")
-
-(bw-add-to-load-path vendor-dotfiles-dir)
-
 ;; built-in modes
 (defconst modes-dotfiles-dir
   (bw-join-dirs dotfiles-dir "modes")
   "Configuration for modes")
 
 (bw-add-to-load-path modes-dotfiles-dir)
-
-;; automatically add everything under vendor to load-path
-(dolist (f (directory-files vendor-dotfiles-dir))
-  (let ((name (concat vendor-dotfiles-dir "/" f)))
-    (when (and (file-directory-p name)
-               (not (equal f ".."))
-               (not (equal f ".")))
-      (bw-add-to-load-path name))))
 
 ;; tmp directory for storing stupid crap
 (make-directory (setq tmp-local-dir (bw-join-dirs dotfiles-dir ".tmp/")) t)
