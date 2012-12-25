34e4ec28c9e49498ca82c0dd8c09e95be98d5103
Bradley Wright <brad@intranation.com>
2012-12-25 22:51:21 +0000

    Never pop a dialogue box on GUI systems

--- init-window-gui.el
+++ init-window-gui.el
@@ -8,6 +8,9 @@
 ;; indicate EOF empty lines in the gutter
 (setq indicate-empty-lines t)
 
+;; never pop a dialogue
+(setq use-dialog-box nil)
+
 ;; From:
 ;; http://emacs-fu.blogspot.co.uk/2011/01/setting-frame-title.html
 (setq frame-title-format
