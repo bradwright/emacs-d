f22ce20a7091c3e58136a54117ed9846ec95f243
Bradley Wright <brad@intranation.com>
2012-12-28 10:22:04 +0000

    Keyboard shortcut for launching eshell

--- modes/init-eshell.el
+++ modes/init-eshell.el
@@ -7,8 +7,12 @@
      (require 'em-term)
      ;; If I try to SSH from an eshell, launch it in ansi-term instead
      (add-to-list 'eshell-visual-commands "ssh")))
+
 ;; fix ANSI colour issues from test runners etc.
 (add-hook 'eshell-preoutput-filter-functions
           'ansi-color-filter-apply)
+
+;; launch an eshell from keys
+(global-set-key (kbd "C-c e") 'eshell)
 
 (provide 'init-eshell)
