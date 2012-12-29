6df6bb42018476eb7de8ce7b83c865b708d563db
Bradley Wright <brad@intranation.com>
2012-12-29 22:49:57 +0000

    Don't run yasnippet in term-mode

--- packages/init-yasnippet.el
+++ packages/init-yasnippet.el
@@ -3,6 +3,11 @@
   :init
   (progn
     (yas-global-mode 1)
+
+    ;; we don't want yasnippet running in terminals
+    (add-hook 'term-mode-hook (lambda()
+                                (yas-minor-mode -1)))
+
     (setq yas/prompt-functions '(yas/ido-prompt yas/completing-prompt))))
 
 (provide 'init-yasnippet)
