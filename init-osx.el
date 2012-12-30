46fadcc7f3ae881fae2400e5324f217920e93830
Bradley Wright <brad@intranation.com>
2012-12-30 09:20:47 +0000

    The extra progn form around these is unnecessary

--- init-osx.el
+++ init-osx.el
@@ -16,75 +16,73 @@
 
 (when (or *is-carbon-emacs*
 	      *is-cocoa-emacs*)
-  (progn
-    ;; Mac GUI stuff
-    ;; set my favourite Mac font as the default font
-    (set-face-attribute 'default nil :foundry "apple"
-                        :family "Inconsolata" :height 160)
+  ;; Mac GUI stuff
+  ;; set my favourite Mac font as the default font
+  (set-face-attribute 'default nil :foundry "apple"
+                      :family "Inconsolata" :height 160)
 
-    ;; (set-face-attribute 'default nil :foundry "apple"
-    ;;                     :family "DejaVu_Sans_Mono" :height 140)
-    ;; (set-face-attribute 'default nil :foundry "adobe"
-    ;;                     :family "Source Code Pro" :height 150)
+  ;; (set-face-attribute 'default nil :foundry "apple"
+  ;;                     :family "DejaVu_Sans_Mono" :height 140)
+  ;; (set-face-attribute 'default nil :foundry "adobe"
+  ;;                     :family "Source Code Pro" :height 150)
 
-    ;; meta key configuration
+  ;; meta key configuration
 
-    ;; This makes left-option do M-
-    (setq ns-alternate-modifier 'meta)
-    ;; ... and right-option just do option so I can still type
-    ;; alternate characters.
-    (setq ns-right-alternate-modifier nil)
+  ;; This makes left-option do M-
+  (setq ns-alternate-modifier 'meta)
+  ;; ... and right-option just do option so I can still type
+  ;; alternate characters.
+  (setq ns-right-alternate-modifier nil)
 
-    ;; command is super
-    (setq ns-command-modifier 'super)
-    ;; fn does nothing special for Emacs
-    (setq ns-function-modifier nil)
+  ;; command is super
+  (setq ns-command-modifier 'super)
+  ;; fn does nothing special for Emacs
+  (setq ns-function-modifier nil)
 
-    (global-set-key (kbd "s-<return>") 'ns-toggle-fullscreen)
+  (global-set-key (kbd "s-<return>") 'ns-toggle-fullscreen)
 
-    ;; we pretty much never ever want to background emacs
-    (global-unset-key (kbd "C-z"))
-    (global-unset-key (kbd "C-x C-z"))
+  ;; we pretty much never ever want to background emacs
+  (global-unset-key (kbd "C-z"))
+  (global-unset-key (kbd "C-x C-z"))
 
-    ;; Don't open files from the workspace in a new frame
-    (setq ns-pop-up-frames nil)
+  ;; Don't open files from the workspace in a new frame
+  (setq ns-pop-up-frames nil)
 
-    ;; Emacs launched from the desktop doesn't inherit the shell
-    ;; env. This package:
-    ;; https://github.com/purcell/exec-path-from-shell automatically
-    ;; mirrors the PATH and other environment variables from a login
-    ;; shell, ensuring that things work correctly.
-    (use-package exec-path-from-shell
-      :init
-      (progn
-        ;; copy SHELL correctly
-        (setq exec-path-from-shell-variables '("PATH" "MANPATH" "SHELL"))
-        ;; copy shell PATH across to exec-path
-        (exec-path-from-shell-initialize)))))
+  ;; Emacs launched from the desktop doesn't inherit the shell
+  ;; env. This package:
+  ;; https://github.com/purcell/exec-path-from-shell automatically
+  ;; mirrors the PATH and other environment variables from a login
+  ;; shell, ensuring that things work correctly.
+  (use-package exec-path-from-shell
+    :init
+    (progn
+      ;; copy SHELL correctly
+      (setq exec-path-from-shell-variables '("PATH" "MANPATH" "SHELL"))
+      ;; copy shell PATH across to exec-path
+      (exec-path-from-shell-initialize))))
 
 (unless (display-graphic-p)
-  (progn
-    ;; Configuration to make Emacs run semi-normally in an OS X
-    ;; terminal
+  ;; Configuration to make Emacs run semi-normally in an OS X
+  ;; terminal
 
-    ;; XXX: strongly recommended to run in iTerm2, as it's more
-    ;; configurable than Terminal.app.
+  ;; XXX: strongly recommended to run in iTerm2, as it's more
+  ;; configurable than Terminal.app.
 
-    ;; Make sure cut/paste works properly. Gotten from:
-    ;; http://mindlev.wordpress.com/2011/06/13/emacs-in-a-terminal-on-osx/#comment-20
-    (defun copy-from-osx ()
-      "Copies the current clipboard content using the `pbcopy` command"
-      (shell-command-to-string "pbpaste"))
+  ;; Make sure cut/paste works properly. Gotten from:
+  ;; http://mindlev.wordpress.com/2011/06/13/emacs-in-a-terminal-on-osx/#comment-20
+  (defun copy-from-osx ()
+    "Copies the current clipboard content using the `pbcopy` command"
+    (shell-command-to-string "pbpaste"))
 
-    (defun paste-to-osx (text &optional push)
-      "Copies the top of the kill ring stack to the OSX clipboard"
-      (let ((process-connection-type nil))
-        (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
-          (process-send-string proc text)
-          (process-send-eof proc))))
+  (defun paste-to-osx (text &optional push)
+    "Copies the top of the kill ring stack to the OSX clipboard"
+    (let ((process-connection-type nil))
+      (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+        (process-send-string proc text)
+        (process-send-eof proc))))
 
-    ;; Override defaults to use the mac copy and paste
-    (setq interprogram-cut-function 'paste-to-osx)
-    (setq interprogram-paste-function 'copy-from-osx)))
+  ;; Override defaults to use the mac copy and paste
+  (setq interprogram-cut-function 'paste-to-osx)
+  (setq interprogram-paste-function 'copy-from-osx))
 
 (provide 'init-osx)
