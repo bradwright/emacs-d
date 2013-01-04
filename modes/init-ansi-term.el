d74eda18c964d343cf214d69e47605f74bf72aea
Bradley Wright <brad@intranation.com>
2013-01-04 21:57:02 +0000

    Kill terminal buffers on exit signal automatically
    Also clean up the advice.

--- modes/init-ansi-term.el
+++ modes/init-ansi-term.el
@@ -1,7 +1,13 @@
 ;; force ansi-term to be utf-8 after it launches
-(defadvice ansi-term
-  (after advise-ansi-term-coding-system)
+(defadvice ansi-term (after advise-ansi-term-coding-system activate)
   (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))
-(ad-activate 'ansi-term)
+
+;; close the terminal buffer automatically on exit
+(defadvice term-sentinel (around my-advice-term-sentinel (proc msg) activate)
+  (if (memq (process-status proc) '(signal exit))
+      (let ((buffer (process-buffer proc)))
+        ad-do-it
+        (kill-buffer buffer))
+    ad-do-it))
 
 (provide 'init-ansi-term)
