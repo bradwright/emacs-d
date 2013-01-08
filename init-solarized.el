3c4189169d6261a9fea98d5990ea7c1297b247da
Bradley Wright <brad@intranation.com>
2013-01-08 22:54:32 +0000

    Simplify swapping themes

--- init-solarized.el
+++ init-solarized.el
@@ -1,16 +1,22 @@
 ;; customise solarized-dark/light themes
+(defun bw/toggle-theme (theme-1 theme-2)
+  "Turn off the current theme and turn on a new one"
+  (cond
+   ((custom-theme-enabled-p theme-1)
+    (--bw-switch-theme theme-2 theme-1))
+   ((custom-theme-enabled-p theme-2)
+    (--bw-switch-theme theme-1 theme-2))))
+
+(defun --bw-switch-theme (theme-to-enable theme-to-disable)
+  (disable-theme theme-to-disable)
+  (when (custom-theme-enabled-p theme-to-enable)
+    (enable-theme theme-to-enable))
+  (load-theme theme-to-enable t))
+
 (defun bw-toggle-solarized ()
   "Toggles between solarized light and dark"
   (interactive)
-  (cond
-   ((custom-theme-enabled-p 'solarized-dark)
-    (progn
-      (disable-theme 'solarized-dark)
-      (enable-theme 'solarized-light)))
-   ((custom-theme-enabled-p 'solarized-light)
-    (progn
-      (disable-theme 'solarized-light)
-      (enable-theme 'solarized-dark)))))
+  (bw/toggle-theme 'solarized-dark 'solarized-light))
 
 ;; We load the theme after init because we might have changed some
 ;; variables in customize.
@@ -30,7 +36,6 @@
       ;; My Macs have the --srgb flag set
       (custom-set-variables
        '(solarized-broken-srgb nil))))
-  (load-theme 'solarized-light t t)
   (load-theme 'solarized-dark t))
 
 (provide 'init-solarized)
