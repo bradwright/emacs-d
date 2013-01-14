cd33fdee7b19e6eb6e41842dfdeae45826934985
Bradley Wright <brad@intranation.com>
2013-01-14 06:55:13 +0000

    Add imenu support to Markdown

--- packages/init-markdown.el
+++ packages/init-markdown.el
@@ -1,5 +1,21 @@
 (use-package markdown-mode
   :mode (("\\.md$" . markdown-mode)
-         ("\\.markdown$" . markdown-mode)))
+         ("\\.markdown$" . markdown-mode))
+  :config
+  (progn
+    (setq markdown-imenu-generic-expression
+      '(("title"  "^\\(.*\\)[\n]=+$" 1)
+        ("h2-"    "^\\(.*\\)[\n]-+$" 1)
+        ("h1"   "^# \\(.*\\)$" 1)
+        ("h2"   "^## \\(.*\\)$" 1)
+        ("h3"   "^### \\(.*\\)$" 1)
+        ("h4"   "^#### \\(.*\\)$" 1)
+        ("h5"   "^##### \\(.*\\)$" 1)
+        ("h6"   "^###### \\(.*\\)$" 1)
+        ("fn"   "^\\[\\^\\(.*\\)\\]" 1)))
+
+    (add-hook 'markdown-mode-hook
+              (lambda ()
+                (setq imenu-generic-expression markdown-imenu-generic-expression)))))
 
 (provide 'init-markdown)
