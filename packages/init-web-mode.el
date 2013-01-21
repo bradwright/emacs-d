2701076030dc52370021b45ba1e5de97631de121
Bradley Wright <brad@intranation.com>
2013-01-21 21:51:50 +0000

    web-mode now handles Jinja2 and Mustache

--- packages/init-web-mode.el
+++ packages/init-web-mode.el
@@ -1,5 +1,7 @@
 (use-package web-mode
   :mode (("\\.html\\'" . web-mode)
-         ("\\.html\\.erb\\'" . web-mode)))
+         ("\\.html\\.erb\\'" . web-mode)
+         ("\\.mustache\\'" . web-mode)
+         ("\\.jinja\\'" . web-mode)))
 
 (provide 'init-web-mode)
