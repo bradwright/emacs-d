4b4c588fe9083dd8e9fe9450332c9e7072dffe4d
Bradley Wright <brad@intranation.com>
2013-01-23 22:17:37 +0000

    Explicitly set browse-kill-ring-replace-yank
    It's the default value, but it may not always be.

--- packages/init-browse-kill-ring.el
+++ packages/init-browse-kill-ring.el
@@ -1,4 +1,6 @@
 (use-package browse-kill-ring
-  :bind ("M-y" . browse-kill-ring))
+  :bind ("M-y" . browse-kill-ring)
+  :config (setq browse-kill-ring-replace-yank t) ; act like yank-pop
+  )
 
 (provide 'init-browse-kill-ring)
