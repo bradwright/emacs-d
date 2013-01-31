279457589cf7dcf0c81b534210cc7787727f65bc
Bradley Wright <brad@intranation.com>
2013-01-31 11:21:06 +0000

    evil: Start in trailing wheels mode

--- packages/init-evil.el
+++ packages/init-evil.el
@@ -1,6 +1,7 @@
 (use-package evil
   :config
   (progn
+    (setq evil-default-state 'emacs)
     (evil-set-initial-state 'git-commit-mode 'insert)
     (evil-set-initial-state 'magit-log-edit-mode 'insert)))
 
