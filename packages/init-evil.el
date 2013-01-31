1926feafeaf18c5b93c99f1c806b49bc17dc9128
Bradley Wright <brad@intranation.com>
2013-01-31 11:22:16 +0000

    evil: Git commits should be Emacs mode

--- packages/init-evil.el
+++ packages/init-evil.el
@@ -2,7 +2,7 @@
   :config
   (progn
     (setq evil-default-state 'emacs)
-    (evil-set-initial-state 'git-commit-mode 'insert)
-    (evil-set-initial-state 'magit-log-edit-mode 'insert)))
+    (evil-set-initial-state 'git-commit-mode 'emacs)
+    (evil-set-initial-state 'magit-log-edit-mode 'emacs)))
 
 (provide 'init-evil)
