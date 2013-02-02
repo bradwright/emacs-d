94282c99472aec3746804cd53cc4b774dc40fcfe
Bradley Wright <brad@intranation.com>
2013-02-02 22:30:37 +0000

    evil: Leader commands

--- packages/init-evil.el
+++ packages/init-evil.el
@@ -5,6 +5,17 @@
     (define-key evil-ex-map "e " 'ido-find-file)
     (define-key evil-ex-map "b " 'ido-switch-buffer)
 
+    ;; leader
+    (let ((evil-leader-map (make-sparse-keymap)))
+      ;; magit status on <leader>g
+      (define-key evil-leader-map "g" 'magit-status)
+      ;; git-ls-files on <leader>l
+      (define-key evil-leader-map "l" 'bw-find-file-git-ls-files-completing)
+      ;; git-grep-files on <leader>s
+      (define-key evil-leader-map "s" 'bw-git-grep)
+      ;; comma is leader
+      (define-key evil-normal-state-map "," evil-leader-map))
+
     (setq evil-default-state 'emacs)
     (evil-set-initial-state 'git-commit-mode 'emacs)
     (evil-set-initial-state 'magit-log-edit-mode 'emacs)))
