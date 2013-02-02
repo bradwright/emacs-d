b44cbdb7fb241e37abf4cc1ba90b4b55ea8c440f
Bradley Wright <brad@intranation.com>
2013-02-02 22:20:19 +0000

    evil: Use ido for :e and :b commands

--- packages/init-evil.el
+++ packages/init-evil.el
@@ -1,6 +1,10 @@
 (use-package evil
   :config
   (progn
+    ;; use ido to open files
+    (define-key evil-ex-map "e " 'ido-find-file)
+    (define-key evil-ex-map "b " 'ido-switch-buffer)
+
     (setq evil-default-state 'emacs)
     (evil-set-initial-state 'git-commit-mode 'emacs)
     (evil-set-initial-state 'magit-log-edit-mode 'emacs)))
