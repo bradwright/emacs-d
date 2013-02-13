53ce0273cda29807c572e620b14e6d2b429a725f
Bradley Wright <brad@intranation.com>
2013-02-13 15:47:43 +0000

    Fix Cucumber step files by using enh-ruby-mode

--- packages/init-enh-ruby.el
+++ packages/init-enh-ruby.el
@@ -1,3 +1,5 @@
-(use-package enh-ruby-mode)
+(use-package enh-ruby-mode
+  ;; enh-ruby-mode gets regular expression step files right
+  :mode ("_steps\\.rb$" . enh-ruby-mode))
 
 (provide 'init-enh-ruby)
