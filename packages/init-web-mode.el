5c1fc567c3de08e925ca209f1703098453f72e2b
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-03-18 22:44:31 +0000

    PHP is also handled by web-mode

--- packages/init-web-mode.el
+++ packages/init-web-mode.el
@@ -2,7 +2,8 @@
   :mode (("\\.html\\'" . web-mode)
          ("\\.html\\.erb\\'" . web-mode)
          ("\\.mustache\\'" . web-mode)
-         ("\\.jinja\\'" . web-mode))
+         ("\\.jinja\\'" . web-mode)
+         ("\\.php\\'" . web-mode))
   :config
   (progn
     (setq web-mode-engines-alist
