09119d05faa34fe66bcc45cb90d34eee5b78754e
Bradley Wright <brad@intranation.com>
2013-03-31 10:27:50 +0100

    Fix Solarized background colours in ansi-term

--- init-solarized.el
+++ init-solarized.el
@@ -23,26 +23,34 @@
 (defun bw-load-solarized ()
   "Loads Solarized light and dark"
   (interactive)
-  (progn
-    (custom-set-faces
-     '(erm-syn-warnline ((t (:box nil))))
-     '(erm-syn-errline  ((t (:box nil))))
-     '(enh-ruby-string-delimiter-face ((t (:foreground "#dc322f" :background nil))))
-     '(enh-ruby-regexp-delimiter-face ((t (:foreground "#dc322f" :background nil))))
-     '(enh-ruby-op-face ((t (:inherit default))))
-     '(magit-diff-add ((t (:inherit diff-added :weight normal))))
-     '(magit-diff-del ((t (:inherit diff-removed :weight normal))))
-     '(diff-refine-change ((t (:inherit diff-refine-change :background nil :weight normal))))
-     '(iedit-occurrence ((t (:inherit lazy-highlight))))
-     '(match ((t (:inherit lazy-highlight :reverse t))))
-     '(erb-face ((t (:background nil))))
-     '(erb-out-delim-face ((t (:inherit erb-exec-delim-face :foreground "#b58900")))))
-    (when (and (display-graphic-p) *is-a-mac*)
-      ;; My Macs have the --srgb flag set
-      ;; so we need to check if it was compiled here.
-      (when (string-match (concat "on " (system-name)) (emacs-version))
-        (custom-set-variables
-         '(solarized-broken-srgb nil)))))
-  (load-theme 'solarized-dark t))
+  (custom-set-faces
+   '(erm-syn-warnline ((t (:box nil))))
+   '(erm-syn-errline  ((t (:box nil))))
+   '(enh-ruby-string-delimiter-face ((t (:foreground "#dc322f" :background nil))))
+   '(enh-ruby-regexp-delimiter-face ((t (:foreground "#dc322f" :background nil))))
+   '(enh-ruby-op-face ((t (:inherit default))))
+   '(magit-diff-add ((t (:inherit diff-added :weight normal))))
+   '(magit-diff-del ((t (:inherit diff-removed :weight normal))))
+   '(diff-refine-change ((t (:inherit diff-refine-change :background nil :weight normal))))
+   '(iedit-occurrence ((t (:inherit lazy-highlight))))
+   '(match ((t (:inherit lazy-highlight :reverse t))))
+   '(erb-face ((t (:background nil))))
+   '(erb-out-delim-face ((t (:inherit erb-exec-delim-face :foreground "#b58900")))))
+  (when (and (display-graphic-p) *is-a-mac*)
+    ;; My Macs have the --srgb flag set
+    ;; so we need to check if it was compiled here.
+    (when (string-match (concat "on " (system-name)) (emacs-version))
+      (custom-set-variables
+       '(solarized-broken-srgb nil))))
+  (load-theme 'solarized-dark t)
+  (custom-set-faces
+   `(term-color-black ((t (:inherit term-color-black :background ,(face-attribute 'term-color-black :foreground)))))
+   `(term-color-red ((t (:inherit term-color-red :background ,(face-attribute 'term-color-red :foreground)))))
+   `(term-color-green ((t (:inherit term-color-green :background ,(face-attribute 'term-color-green :foreground)))))
+   `(term-color-yellow ((t (:inherit term-color-yellow :background ,(face-attribute 'term-color-yellow :foreground)))))
+   `(term-color-blue ((t (:inherit term-color-blue :background ,(face-attribute 'term-color-blue :foreground)))))
+   `(term-color-magenta ((t (:inherit term-color-magenta :background ,(face-attribute 'term-color-magenta :foreground)))))
+   `(term-color-cyan ((t (:inherit term-color-cyan :background ,(face-attribute 'term-color-cyan :foreground)))))
+   `(term-color-white ((t (:inherit term-color-white :background ,(face-attribute 'term-color-white :foreground)))))))
 
 (provide 'init-solarized)
