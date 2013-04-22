07a87a9faa2608f609baf815f8df773fc3474dab
Bradley Wright <brad@intranation.com>
2013-04-22 12:55:56 +0100

    Enable paredit in the minibuffer

--- packages/init-paredit.el
+++ packages/init-paredit.el
@@ -4,6 +4,13 @@
     ;; change keyboard commands only in terminal mode
     (when (not (display-graphic-p))
       (define-key paredit-mode-map (kbd "M-)") 'paredit-forward-slurp-sexp)
-      (define-key paredit-mode-map (kbd "M-(") 'paredit-backward-slurp-sexp))))
+      (define-key paredit-mode-map (kbd "M-(") 'paredit-backward-slurp-sexp))
+
+    ;; Enable `paredit-mode' in the minibuffer, during `eval-expression'.
+    (defun conditionally-enable-paredit-mode ()
+      (if (eq this-command 'eval-expression)
+          (paredit-mode 1)))
+
+    (add-hook 'minibuffer-setup-hook 'conditionally-enable-paredit-mode)))
 
 (provide 'init-paredit)
