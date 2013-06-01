2ef83b42724aa068c5c0d173a833316f346d6d36
Bradley Wright <brad@intranation.com>
2013-06-01 21:53:48 +0100

    Make edit-server work with use-package

--- packages/init-edit-server.el
+++ packages/init-edit-server.el
@@ -1,6 +1,7 @@
-(when (require 'edit-server nil t)
-  (edit-server-start))
-(setq edit-server-url-major-mode-alist
-      '(("github\\.com" . markdown-mode)))
+(use-package edit-server
+  :init (progn
+          (edit-server-start)
+          (setq edit-server-url-major-mode-alist
+                '(("github\\.com" . markdown-mode)))))
 
 (provide 'init-edit-server)
