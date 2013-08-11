85afbf37b4fc346b201f86990a26167f554fd1b0
Bradley Wright <brad@intranation.com>
2013-08-11 16:22:56 +0100

    Evil: use evil-leader, boot evil-mode by default
    I start in Emacs mode now, so booting Evil actually doesn't change the
    day to day of editing. It does mean I can switch to normal mode when I
    want.
    
    Also replace my customised leader stuff with the evil-leader package.

--- packages/init-evil.el
+++ packages/init-evil.el
@@ -2,36 +2,37 @@
   :init
   (progn
     ;; if we don't have this evil overwrites the cursor color
-    (setq evil-default-cursor t))
+    (setq evil-default-cursor t)
+
+    ;; leader shortcuts
+    (use-package evil-leader
+      :init (global-evil-leader-mode)
+      :config
+      (progn
+        ;; keyboard shortcuts
+        (evil-leader/set-key
+         "e" 'ido-find-file
+         "b" 'ido-switch-buffer
+         "g" 'magit-status
+         "l" 'bw-find-file-git-ls-files-completing
+         "s" 'bw-git-grep
+         "x" 'smex
+         "a" 'ack-and-a-half)))
+
+    ;; boot evil by default
+    (evil-mode 1))
   :config
   (progn
     ;; use ido to open files
     (define-key evil-ex-map "e " 'ido-find-file)
     (define-key evil-ex-map "b " 'ido-switch-buffer)
 
-    ;; leader
-    (let ((evil-leader-map (make-sparse-keymap)))
-      ;; magit status on <leader>g
-      (define-key evil-leader-map "g" 'magit-status)
-      ;; git-ls-files on <leader>l
-      (define-key evil-leader-map "l" 'bw-find-file-git-ls-files-completing)
-      ;; git-grep-files on <leader>s
-      (define-key evil-leader-map "s" 'bw-git-grep)
-      ;; smex is <leader>x
-      (define-key evil-leader-map "x" 'smex)
-      ;; ido-switch-buffer is <leader>b
-      (define-key evil-leader-map "b" 'ido-switch-buffer)
-      ;; ack-and-a-half
-      (define-key evil-leader-map "a" 'ack-and-a-half)
-      ;; comma is leader
-      (define-key evil-normal-state-map "," evil-leader-map))
-
     ;; jj escapes to normal mode
     (define-key evil-insert-state-map (kbd "j") 'bw-evil-escape-if-next-char-is-j)
     ;; h/l wrap around to next lines
     (setq evil-cross-lines t)
-    ;; start evil-mode in normal
-    (setq evil-default-state 'normal)
+    ;; start evil-mode in emacs mode
+    (setq evil-default-state 'emacs)
 
     ;; esc should always quit: http://stackoverflow.com/a/10166400/61435
     (define-key evil-normal-state-map [escape] 'keyboard-quit)
