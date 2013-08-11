b5c1b666375d98bb1201b2b4c6161c8e555607c6
Bradley Wright <brad@intranation.com>
2013-08-11 23:15:19 +0100

    Add my own enh-ruby-mode expand region setting

--- packages/init-expand-region.el
+++ packages/init-expand-region.el
@@ -1,4 +1,13 @@
 (use-package expand-region
+  :config
+  (progn
+    (defun er/add-enh-ruby-mode-expansions ()
+      "Adds Ruby-specific expansions for buffers in enh-ruby-mode"
+      (set (make-local-variable 'er/try-expand-list) (append
+                                                      (remove 'er/mark-defun er/try-expand-list)
+                                                      '(enh-ruby-mark-defun))))
+
+    (er/enable-mode-expansions 'enh-ruby-mode 'er/add-enh-ruby-mode-expansions))
   :bind ("C-c =" . er/expand-region))
 
 (provide 'init-expand-region)
