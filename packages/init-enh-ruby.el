1cd7dd5ed423312865d0c74ff9d0dd6d9ea545a8
Bradley Wright <brad@intranation.com>
2013-08-13 16:48:35 +0100

    Set some faces in enh-ruby-mode

--- packages/init-enh-ruby.el
+++ packages/init-enh-ruby.el
@@ -11,6 +11,13 @@
   :config
   (progn
     ;; Ruby has a lot of camel case
-    (add-hook 'enh-ruby-mode-hook 'bw-turn-on-subword-mode)))
+    (add-hook 'enh-ruby-mode-hook 'bw-turn-on-subword-mode)
+    (custom-set-faces
+     '(erm-syn-errline ((t (:box nil))))
+     '(erm-syn-warnline ((t (:box nil))))
+     '(enh-ruby-op-face ((t (:inherit default))))
+     '(enh-ruby-string-delimiter-face ((t (:foreground "#dc322f" :background nil))))
+     '(enh-ruby-regexp-delimiter-face ((t (:foreground "#dc322f" :background nil))))
+     '(enh-ruby-heredoc-delimiter-face ((t (:foreground "#dc322f" :background nil)))))))
 
 (provide 'init-enh-ruby)
