2e9044f7afb411004ef0727670607a93b8651abe
Bradley Wright <brad@intranation.com>
2013-08-19 15:35:09 +0100

    Fix multiple-cursor keybindings
    Lazily loading them didn't work until after they'd been run once -
    explicitly setting them with global-set-key fixes this.

--- packages/init-multiple-cursors.el
+++ packages/init-multiple-cursors.el
@@ -1,9 +1,12 @@
 (use-package multiple-cursors
-  :init (progn )
-  :bind (("C-c ." . mc/mark-next-like-this)
-         ("C->" . mc/mark-next-like-this)
-         ("C-c ," . mc/mark-previous-like-this)
-         ("C-<" . mc/mark-previous-like-this)
-         ("C-c C-l". mc/mark-all-like-this)))
+  :init
+  (progn
+    ;; these need to be defined here - if they're lazily loaded with
+    ;; :bind they don't work.
+    (global-set-key (kbd "C-c .") 'mc/mark-next-like-this)
+    (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+    (global-set-key (kbd "C-c ,") 'mc/mark-previous-like-this)
+    (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+    (global-set-key (kbd "C-c C-l") 'c/mark-all-like-this)))
 
 (provide 'init-multiple-cursors)
