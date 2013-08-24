b0d78ff1c2bd482a83db319e6d3b358f7019fce0
Bradley Wright <brad@intranation.com>
2013-08-24 23:01:03 +0100

    Don't use ack-and-a-half any more

--- .dir-locals.el
+++
@@ -1,4 +0,0 @@
-((nil . ((eval . (eval-after-load 'ack-and-a-half
-                   '(progn
-                      (add-to-list 'ack-and-a-half-arguments "--ignore-dir=elpa/installed")
-                      (add-to-list 'ack-and-a-half-arguments "--ignore-dir=vendor")))))))
